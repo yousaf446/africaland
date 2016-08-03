7922b26a83922f97d4a0648a44d48ba2630589a4
yousaf446 <yousaf_bilal91@yahoo.com>
2016-08-03 12:54:16 +0500

    Content Menu & Get Directions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var map;
+var marker;
 var infowindow;
 var geocoder = new google.maps.Geocoder();
 var suggestions = [];
@@ -6,6 +7,9 @@ var w3w_key = "ABN6G5I3";
 var selectedID = "";
 var pyrmont = {lat: -33.867, lng: 151.195};
 var mapType = 'map';
+var contextmenuDir;
+var pinDrag = true;
+var destination;
 function loadMap() {
 
     var mapTypeStyle = google.maps.MapTypeControlStyle.HORIZONTAL_BAR;
@@ -45,6 +49,10 @@ function loadMap() {
 
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+    marker = new google.maps.Marker({
+        map: map,
+    });
+    destination = new google.maps.LatLng(pyrmont.lat, pyrmont.lng);
     infowindow = new google.maps.InfoWindow();
 
     var search = document.getElementById('search');
@@ -89,6 +97,8 @@ function loadMap() {
         updateOverlays();
         updateAttribution();
     });
+
+    google.maps.event.addListener(map, "rightclick",function(event){showContextMenu(event.latLng);});
 }
 
 function googleSuggestions() {
@@ -116,10 +126,7 @@ function geocodeAddress() {
     geocoder.geocode({'address': address}, function(results, status) {
         if (status === 'OK') {
             map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
+            marker.setPosition(results[0].geometry.location);
             reverseGeoCode3WordsGMaps(results[0].geometry.location);
         }
     });
@@ -211,10 +218,7 @@ function filterSuggestion() {
 
 function plotMarker(geometry) {
     map.setCenter(geometry);
-    var marker = new google.maps.Marker({
-        map: map,
-        position: geometry
-    });
+    marker.setPosition(geometry);
 }
 
 function reverseGeoCode3Words(location) {
@@ -239,6 +243,36 @@ function reverseGeoCode3WordsGMaps(location) {
         }
     });
 
+}
+
+function PlacePin(lat, lng) {
+    var location = new google.maps.LatLng(lat, lng);
+    reverseGeoCode3WordsGMaps(location);
+    if(pinDrag) {
+        marker.setPosition(location);
+    }
+    else {
+        map.setCenter(location);
+        marker.setPosition(map.getCenter());
+    }
+    contextmenuDir.style.visibility = "hidden";
+}
+
+function PinDragCheck(check) {
+    console.log(check);
+    pinDrag = check;
+    if(pinDrag) {
+        $("#menu2").show();
+        $("#menu3").hide();
+    } else {
+        $("#menu2").hide();
+        $("#menu3").show();
+    }
+    contextmenuDir.style.visibility = "hidden";
+}
+
+function HideContextMenu() {
+    contextmenuDir.style.visibility = "hidden";
 }
 
 function forwardGeoCode3Words(address) {
@@ -285,6 +319,59 @@ window.mobilecheck = function() {
     var check = false;
     (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
+}
+
+function showContextMenu(currentLatLng) {
+    var projection;
+    projection = map.getProjection() ;
+    $('.contextmenu').remove();
+    contextmenuDir = document.createElement("div");
+    contextmenuDir.className  = 'contextmenu';
+    var content = '<a id="menu1" href="javascript:void(0)" onclick="PlacePin('+currentLatLng.lat()+','+currentLatLng.lng()+')"><div class="context">Place Pin Here<\/div><\/a>';
+    if(pinDrag) content += '<a id="menu2"><div class="context" href="javascript:void(0)" onclick="PinDragCheck(false)">Lock Pin<\/div><\/a>';
+    else content += '<a id="menu3" href="javascript:void(0)" onclick="PinDragCheck(true)"><div class="context">UnLock Pin<\/div><\/a>';
+    content += '<a id="menu4" href="https://www.google.com/maps?saddr=My+Location&daddr='+destination+'" onclick="HideContextMenu()" target="_blank"><div class="context">Directions To Pin<\/div><\/a>';
+
+    contextmenuDir.innerHTML = content;
+    $(map.getDiv()).append(contextmenuDir);
+
+    setMenuXY(currentLatLng);
+
+    contextmenuDir.style.visibility = "visible";
+    destination = currentLatLng.lat() + "," + currentLatLng.lng();
+}
+
+function getCanvasXY(currentLatLng){
+    var scale = Math.pow(2, map.getZoom());
+    var nw = new google.maps.LatLng(
+        map.getBounds().getNorthEast().lat(),
+        map.getBounds().getSouthWest().lng()
+    );
+    var worldCoordinateNW = map.getProjection().fromLatLngToPoint(nw);
+    var worldCoordinate = map.getProjection().fromLatLngToPoint(currentLatLng);
+    var currentLatLngOffset = new google.maps.Point(
+        Math.floor((worldCoordinate.x - worldCoordinateNW.x) * scale),
+        Math.floor((worldCoordinate.y - worldCoordinateNW.y) * scale)
+    );
+    return currentLatLngOffset;
+}
+
+function setMenuXY(caurrentLatLng){
+    var mapWidth = $('#map_canvas').width();
+    var mapHeight = $('#map_canvas').height();
+    var menuWidth = $('.contextmenu').width();
+    var menuHeight = $('.contextmenu').height();
+    var clickedPosition = getCanvasXY(caurrentLatLng);
+    var x = clickedPosition.x ;
+    var y = clickedPosition.y ;
+
+    if((mapWidth - x ) < menuWidth)//if to close to the map border, decrease x position
+        x = x - menuWidth;
+    if((mapHeight - y ) < menuHeight)//if to close to the map border, decrease y position
+        y = y - menuHeight;
+
+    $('.contextmenu').css('left',x  );
+    $('.contextmenu').css('top',y );
 }
 
 google.maps.event.addDomListener(window, 'load', loadMap);
